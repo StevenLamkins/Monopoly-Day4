ad9e2801c9f7502f96e3c74c20ec8c947fb907e1
Don McGreal <mail@donmcgreal.com>
2015-12-15 12:00:02 -0600

    added square, game, player classes and junit tests

---
+++ Monopoly/src/com/monopoly/Player.java
@@ -0,0 +1,6 @@
+package com.monopoly;
+
+public class Player {
+	private String name;
+	private int money;
+}
---
+++ Monopoly/src/com/monopoly/Square.java
@@ -0,0 +1,9 @@
+package com.monopoly;
+
+public class Square {
+	private boolean occupied = false;
+	private String name;
+	private int cost;
+	private int rent;
+	
+}
