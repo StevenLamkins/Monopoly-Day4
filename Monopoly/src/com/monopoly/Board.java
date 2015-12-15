8e94ef1b7ff08586ae064a768a383c53eeababb9
customer <customer@10.120.0.28>
2015-12-15 11:41:52 -0600

    Added file read capability to obtain id and square type to board

--- Monopoly/src/com/monopoly/Board.java
+++ Monopoly/src/com/monopoly/Board.java
@@ -1,5 +1,7 @@
 package com.monopoly;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +11,24 @@ public class Board {
 	List<Square> squares = new ArrayList<>();
 	
 	public Board(){
-		for (int i = 0; i < BOARD_SQUARE_COUNT; i++) {
-			Square newSquare = new Square(Integer.toString(i)) ;
-			squares.add(newSquare); 
-		}
+		readBoard();
 	}//end Board constructor
+	
+	public void readBoard(){
+		try {
+			List<String> lines = Files.readAllLines(Paths.get("config/BoardInfo.prop"));
+			for (String line : lines) {
+				
+				String id = line.split(",")[0].split(":")[0];
+				String squareType = line.split(",")[1].split(":")[1];
+				
+				Square newSquare = new Square(id,squareType);
+				squares.add(newSquare);
+			}//end for each
+		} catch (Exception e) {
+			e.printStackTrace();
+		}//end catch
+	}//end readBoard
 
 	public List<Square> getSquares() {
 		return squares;
--- Monopoly/src/com/monopoly/Square.java
+++ Monopoly/src/com/monopoly/Square.java
@@ -3,9 +3,11 @@ package com.monopoly;
 public class Square {
 
 	String id;
+	String squareType;
 	
-	public Square(String id){
+	public Square(String id,String squareType){
 		this.id=id;
+		this.squareType =squareType;
 	}//end Square Constructor
 	
 }//end Square
