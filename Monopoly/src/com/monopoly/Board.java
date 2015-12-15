978b0a3bbcd2a48902dfb2e74b580a6d8b49cb20
Andy Nguyen <customer@PC13146>
2015-12-15 13:38:30 -0600

    Added changes to Square class

--- Monopoly/src/com/monopoly/Board.java
+++ Monopoly/src/com/monopoly/Board.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+	
 	List<Square> squares = new ArrayList<>();
 	
-	public Board() {
-		for (int i = 0; i < 40; i++) {
+	public Board() 
+	{
+		for (int i = 0; i < 40; i++) 
+		{
 			squares.add(new Square());
 		}
 	}
 
-	public List<Square> getSquares() {
+	public List<Square> getSquares() 
+	{
 		return squares;
 	}
 
--- Monopoly/src/com/monopoly/Square.java
+++ Monopoly/src/com/monopoly/Square.java
@@ -2,4 +2,57 @@ package com.monopoly;
 
 public class Square {
 
+	public final int NO_COLOR = 0;
+	public final int BLACK = 1;
+	public final int WHITE = 2;
+	
+
+	private String horizonLine = "---------";
+	private String verticaLine = "|";
+	private String plusSign = "+";
+	private String space = "         ";
+	
+	//-------------------------------
+	// Class attributes
+	//-------------------------------
+	private int position;   // The position of the square (0...39)
+	private int color;      // The square color
+	private int name;       // The square name
+	
+	//-------------------------------
+	// Public functions
+	//-------------------------------
+	public void show() {
+	    System.out.println(plusSign + horizonLine + plusSign);	
+	    System.out.println(verticaLine + space + verticaLine);	
+	    System.out.println(verticaLine + space + verticaLine);	
+	    System.out.println(plusSign + horizonLine + plusSign);	
+	}
+	
+	public void doShow() {
+	    System.out.println("+---------+---------+---------+---------+---------+");	
+		System.out.println("|         |         |         |         |         |");
+		System.out.println("|         |         |         |         |         |");
+		System.out.println("+---------+---------+---------+---------+---------+");	
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int pos) {
+		this.position = pos;
+	}
+	
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}	
+	
+	
+	
 }
