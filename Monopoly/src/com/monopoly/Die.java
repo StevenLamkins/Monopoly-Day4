b10fca6c4da81cce7b597f6b3132d65e7adba50e
Matt <mborders@outlook.com>
2015-12-15 10:21:06 -0600

    Added Die class

---
+++ Monopoly/src/com/monopoly/Die.java
@@ -0,0 +1,35 @@
+package com.monopoly;
+
+import java.util.Random;
+
+public class Die {
+	private static Random numGen = new Random();
+	private int numSides;
+	private int faceValue;
+	
+	public Die() {
+		this(6);
+	}
+	
+	public Die(int numSides) throws IllegalArgumentException {
+		if (numSides < 2) throw new IllegalArgumentException("Too few sides!");
+		
+		this.numSides = numSides;
+		
+		roll();
+	}
+	
+	public int getFaceValue() {
+		return faceValue;
+	}
+	
+	public int getNumSides() {
+		return numSides;
+	}
+	
+	public int roll() {
+		faceValue = numGen.nextInt(this.numSides) + 1;
+		
+		return faceValue;
+	}
+}
