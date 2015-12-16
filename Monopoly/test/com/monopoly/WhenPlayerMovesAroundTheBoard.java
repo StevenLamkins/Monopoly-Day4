3b3838ac0882b74c278df9ae1a8f1b3982df4daa
Mark Dologuin <customer@10.120.0.47>
2015-12-16 09:34:05 -0600

    Deleted redundant test.

--- Monopoly/test/com/monopoly/WhenPlayerMovesAroundTheBoard.java
+++ Monopoly/test/com/monopoly/WhenPlayerMovesAroundTheBoard.java
@@ -4,14 +4,15 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class WhenPlayerMovesAroundTheBoard {
 
-
 	@Test
-	public void playerShouldMoveCorrectNumberOfSquares() {
+	public void playerShouldMoveCorrectNumberOfSquares() 
+	{
 		
 		for (int x = 2; x < 9; x++) {
 			MonopolyGame game = new MonopolyGame(x);
@@ -24,10 +25,7 @@ public class WhenPlayerMovesAroundTheBoard {
 			testPlayer.move(45);
 			expectedSquare = squares.get(5);
 			assertEquals(expectedSquare, testPlayer.getCurrentPosition());
-			
 
-				
-			
 		}
 		
 	}
@@ -40,21 +38,22 @@ public class WhenPlayerMovesAroundTheBoard {
 		Player testPlayer = game.getPlayers().get(0);
 		
 		Square expectedSquare = game.getSquares().get(5);
-		testPlayer.move(5);
+		testPlayer.move(45);
 		
 		assertEquals(expectedSquare,testPlayer.getCurrentPosition());
 		
 	}
 	
-	@Ignore
 	@Test
-	public void playerLavesGo(){
-		fail("Not yet implemented");
+	public void playerLeavesGo(){
+		MonopolyGame game = new MonopolyGame(2);
+		Player testPlayer = game.getPlayers().get(0);
+		
+		Square expectedSquare = game.getSquares().get(1);
+		testPlayer.move(1);
+		
+		assertEquals(expectedSquare,testPlayer.getCurrentPosition());
 	}
 	
-	@Ignore
-	@Test
-	public void playerRollsSameAsMovement(){
-		fail("Not yet implemented");
-	}
+
 }
