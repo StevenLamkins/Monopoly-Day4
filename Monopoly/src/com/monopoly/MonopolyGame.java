8d666c6c741fd9e7490acb6bdc3f21f88be47f38
customer <customer@10.120.0.25>
2015-12-16 14:17:03 -0600

    game averages

--- Monopoly/src/com/monopoly/MonopolyGame.java
+++ Monopoly/src/com/monopoly/MonopolyGame.java
@@ -2,8 +2,10 @@ package com.monopoly;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MonopolyGame {
@@ -24,7 +26,7 @@ public class MonopolyGame {
 		else if (playerCount>8){
 			throw new IllegalArgumentException("Too many players");
 		}//end if
-		
+		players = new ArrayList<Player>();
 		for (int i = 0; i < playerCount; i++) {
 			players.add(createPlayer(i));
 		}
@@ -92,35 +94,65 @@ public class MonopolyGame {
 			}
 		}
 		else{
-			System.out.println("The game is over! The " + players.get(0).getToken() + " has won!");
+			if(players.isEmpty())
+				System.out.println("The game is over! No one won!");
+			else
+				System.out.println("The game is over! The " + players.get(0).getToken() + " has won!");
 			gameRunning = false;
 		}
 	}
 	
-	public static void main(String[] args) {
-		MonopolyGame game = new MonopolyGame(3);
-		Scanner keyboard = new Scanner(System.in);
-		int counter = 0;
-		while(game.getGameRunning()){
+	public int runGame(Map<String, Integer> turnMap, int attempt){
+		int counter=0;
+		boolean tooRichToPlay=true;
+		while(this.getGameRunning()){
+			tooRichToPlay=true;
 			StringBuilder output = new StringBuilder();
-			for (Player player : game.getPlayers()) {
+			for (Player player : players) {
 				if(player.getToken().equals("Thimbal") || player.getToken().equals("WheelBarrow")){
 					output.append(player.getToken()+":\t\tPrevious Roll - " + player.getPreviousRoll() + "\tMoney - "+player.getMoney() + "\tProperty Count - "+player.ownedProperties.size()+"\tSquare - " + player.getSquare().getId()+ "\n");
 				}
 				else{
 					output.append(player.getToken()+":\t\t\tPrevious Roll - " + player.getPreviousRoll() + "\tMoney - "+player.getMoney() + "\tProperty Count - "+player.ownedProperties.size()+"\tSquare - " + player.getSquare().getId()+"\n");
 				}
+				if(player.getMoney()<5000)
+					tooRichToPlay=false;
 			}
 			System.out.println(output.toString());
 			System.out.println("Turn: " + counter++ + "--------------------------------ENTER TO CONTINUE---------------------------------------");
-			try {
-				System.in.read();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-//			keyboard.next();
-			game.runRound();
+			this.runRound();
+			if(tooRichToPlay)
+				this.gameRunning=false;
 		}
+		if(tooRichToPlay){
+			turnMap.put("Rich"+Integer.toString(attempt), counter);
+		}
+		else{
+			turnMap.put("Monopoly"+Integer.toString(attempt), counter);
+		}
+		return counter;
+	}
+	
+	public static void main(String[] args) {
+		double turnCounter=0;
+		Map<String, Integer> turnMap = new HashMap<String, Integer>(); 
+		for(int i=0; i<100; i++){
+			MonopolyGame game = new MonopolyGame(4);
+			turnCounter+=game.runGame(turnMap, i);
+		}
+		double richEnding=0, monopolyEnding=0, richAverage=0, monopolyAverage=0;
+		for (String key : turnMap.keySet()) {
+			if(key.contains("Rich")){
+				richEnding++;
+				richAverage+=turnMap.get(key);
+			}
+			else{
+				monopolyEnding++;
+				monopolyAverage+=turnMap.get(key);
+			}
+		}
+		System.out.println("Rich ending count: " + richEnding + " with an average turn count of: " + richAverage/richEnding);
+		System.out.println("Monopoly ending count: " + monopolyEnding + " with an average turn count of: " + monopolyAverage/monopolyEnding);
 	}//end main
 	
 	
