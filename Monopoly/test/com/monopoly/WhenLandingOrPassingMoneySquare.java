cb9556e45ac6d9fd628ded3182e222aaa041ac54
customer <customer@10.120.0.22>
2015-12-16 11:22:07 -0600

    added landing on square behavior

---
+++ Monopoly/test/com/monopoly/WhenLandingOrPassingMoneySquare.java
@@ -0,0 +1,19 @@
+package com.monopoly;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhenLandingOrPassingMoneySquare {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
