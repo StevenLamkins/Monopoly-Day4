0e7cdf81989632dd5f17be8e2995c522882b6a78
team03 <team03@PC13188>
2015-12-16 16:32:50 -0600

    Added test to check for player paying rent to owner.

--- Monopoly/test/com/monopoly/WhenLandingOnPropertySquare.java
+++ Monopoly/test/com/monopoly/WhenLandingOnPropertySquare.java
@@ -13,7 +13,7 @@ public class WhenLandingOnPropertySquare {
 
 	@Test
 	public void shouldBuyPropertyWhenHaveFundsAndNotOwned() {
-		Board board = new Board();	
+		Board board = new Board();
 
 		Player player = new Player(board.getSquares().get(0), 1);
 
@@ -23,38 +23,41 @@ public class WhenLandingOnPropertySquare {
 		List<Die> dice = new ArrayList<>();
 		dice.add(die1);
 		dice.add(die2);
-  		player.takeTurn(dice);
-  		System.out.println(player.getSquare().getName() + " balance  " + player.getAccountBalance());
-  		((PropertySquare)player.getSquare()).getPrice();
-		
-  		assertEquals(startingBalance - 60, player.getAccountBalance(), 0.001);
-  		assertEquals(player, ((PropertySquare)player.getSquare()).getOwner());
+		player.takeTurn(dice);
+		System.out.println(player.getSquare().getName() + " balance  "
+				+ player.getAccountBalance());
+		((PropertySquare) player.getSquare()).getPrice();
+
+		assertEquals(startingBalance - 60, player.getAccountBalance(), 0.001);
+		assertEquals(player, ((PropertySquare) player.getSquare()).getOwner());
 
 	}
 
 	@Test
 	public void notBuyPropertyThatIsOwned() {
-		Board board = new Board();	
+		Board board = new Board();
 
 		Player player = new Player(board.getSquares().get(0), 1);
 		Player player2 = new Player(board.getSquares().get(0), 2);
-		
+
 		Die die1 = new LoadedDie(2);
 		Die die2 = new LoadedDie(1);
 		List<Die> dice = new ArrayList<>();
 		dice.add(die1);
-		dice.add(die2);		
-  		player.takeTurn(dice);
-  		player2.takeTurn(dice);
-  		System.out.println(player.getSquare().getName() + " balance  " + player.getAccountBalance());
-  		((PropertySquare)player.getSquare()).getPrice();		
-  		
-  		assertNotEquals(player2, ((PropertySquare)player.getSquare()).getOwner());
+		dice.add(die2);
+		player.takeTurn(dice);
+		player2.takeTurn(dice);
+		System.out.println(player.getSquare().getName() + " balance  "
+				+ player.getAccountBalance());
+		((PropertySquare) player.getSquare()).getPrice();
+
+		assertNotEquals(player2,
+				((PropertySquare) player.getSquare()).getOwner());
 	}
-	
+
 	@Test
 	public void playerDoesNotBuyPropertyCannotAfford() {
-		Board board = new Board();	
+		Board board = new Board();
 
 		Player player = new Player(board.getSquares().get(0), 1);
 
@@ -64,35 +67,36 @@ public class WhenLandingOnPropertySquare {
 		List<Die> dice = new ArrayList<>();
 		dice.add(die1);
 		dice.add(die2);
-  		player.takeTurn(dice);
-  		System.out.println(player.getSquare().getName() + " balance  " + player.getAccountBalance());
-  		((PropertySquare)player.getSquare()).getPrice();
-		
-  		assertEquals(10, player.getAccountBalance(), 0.001);
-  		assertNotEquals(player, ((PropertySquare)player.getSquare()).getOwner());
+		player.takeTurn(dice);
+		System.out.println(player.getSquare().getName() + " balance  "
+				+ player.getAccountBalance());
+		((PropertySquare) player.getSquare()).getPrice();
+
+		assertEquals(10, player.getAccountBalance(), 0.001);
+		assertNotEquals(player,
+				((PropertySquare) player.getSquare()).getOwner());
 	}
-	
+
 	@Test
 	public void playerShouldPaysRentToOwner() {
-		Board board = new Board();	
+		Board board = new Board();
 
 		Player player = new Player(board.getSquares().get(0), 1);
 		Player player2 = new Player(board.getSquares().get(0), 2);
-		
+
 		Die die1 = new LoadedDie(2);
 		Die die2 = new LoadedDie(4);
 		List<Die> dice = new ArrayList<>();
 		dice.add(die1);
-		dice.add(die2);		
-  		player.takeTurn(dice);
-  		player2.takeTurn(dice);
-  		System.out.println(player.getSquare().getName() + " balance  " + player.getAccountBalance());
-  		((PropertySquare)player.getSquare()).getPrice();		
-  		 
-  		assertEquals(1436,player.getAccountBalance(), 0.001);
-  		assertEquals(1494, player2.getAccountBalance(), 0.001);
+		dice.add(die2);
+		player.takeTurn(dice);
+		player2.takeTurn(dice);
+		System.out.println(player.getSquare().getName() + " balance  "
+				+ player.getAccountBalance());
+		((PropertySquare) player.getSquare()).getPrice();
+
+		assertEquals(1436, player.getAccountBalance(), 0.001);
+		assertEquals(1494, player2.getAccountBalance(), 0.001);
 
 	}
-	
-	
 }
