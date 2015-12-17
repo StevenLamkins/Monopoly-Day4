62f6dd71956daecc5307ae56eae28e546d70c59d
team03 <team03@PC13188>
2015-12-17 13:50:13 -0600

    new app

--- Monopoly/src/com/monopoly/ui/MonopolyApp.java
+++ Monopoly/src/com/monopoly/ui/MonopolyApp.java
@@ -15,15 +15,54 @@ public class MonopolyApp {
 		}
 		List<PlayerInfo> players = game.getPlayers();
 
-		for (int i = 0; i < 10; i++) {
-			game.playRound();
-			for (PlayerInfo player : players) {
-				System.out.println(player.getToken() + " rolled a " + player.getLastRoll());
-				System.out.println(player.getToken() + " land on " + player.getSquare().getName());
-				System.out.println(player.getToken() + " has $" + player.getAccountBalance());
-			}
-			System.out.println("-----------------------");
-		}
+
+
+		 for (int i = 0; i < 10; i++) {
+		 game.playRound();
+		 for (PlayerInfo player : players) {
+		 System.out.println(player.getToken() + " rolled a " +
+		 player.getLastRoll());
+		 System.out.println(player.getToken() + " land on " +
+		 player.getSquare().getName());
+		 System.out.println(player.getToken() + " has $" +
+		 player.getAccountBalance());
+		 }
+		 System.out.println("-----------------------");
+		 }
+		 
+		 
+//To simulate a full game must change go square to collect $20
+//		int countBalanceNotNegative = game.getPlayers().size();
+//		int round = 0;
+//		while (countBalanceNotNegative > 1) {
+//			countBalanceNotNegative = 0;
+//			for (PlayerInfo playerInfo : players) {
+//				if (playerInfo.getAccountBalance() > 0)
+//					countBalanceNotNegative++;
+//			}
+//			game.playRound();
+//			for (PlayerInfo player : players) {
+//				// System.out.println(player.getToken() + " rolled a " +
+//				// player.getLastRoll());
+//				// System.out.println(player.getToken() + " land on " +
+//				// player.getSquare().getName());
+//				System.out.println(player.getToken() + " has $"
+//						+ player.getAccountBalance());
+//			}
+//			round++;
+//			System.out.println("--------Round " + round + "---------------");
+//		}
+//		String winner = "nobody";
+//		for (PlayerInfo player : players) {
+//			System.out.println(player.getToken() + " has $"
+//					+ player.getAccountBalance());
+//			if(player.getAccountBalance() > 0)
+//			{
+//				winner=player.getToken().name();
+//			}			
+//		}
+//		System.out.println("The Winner is " + winner + "!!");
+
 
 	}
 
